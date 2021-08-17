3f144996615b2a972bf9899041dd1e5daefd1d70
SanchitMahadik <83035088+SanchitMahadik@users.noreply.github.com>
2021-08-17 19:13:01 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-$(document).ready(function () {
-    $('.card-wrapper').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-      });
-
-      $(".play_video").click(function () { 
-        Swal.fire({
-            html: '<iframe width="500" height="300" src="https://www.youtube.com/embed/yChiMuzNOIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-        });
-      });
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2000
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
