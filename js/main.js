a18a23d82bc79f5293cc3bcb1d51f4bf34857eb6
SanchitMahadik <83035088+SanchitMahadik@users.noreply.github.com>
2021-08-17 23:49:22 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -2,12 +2,25 @@ const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 3,
   slidesPerGroup: 1,
-  spaceBetween: 30,
   autoplay: {
     delay: 2000
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 35,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
